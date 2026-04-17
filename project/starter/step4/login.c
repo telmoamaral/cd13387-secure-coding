fd3a37e506661e663cf817b5d290169d118490b2
Telmo Amaral <22940434+telmoamaral@users.noreply.github.com>
2026-04-17 19:22:06 +0000

    Step4 fixed attempts and blocking timer mechanisms

--- project/starter/step4/login.c
+++ project/starter/step4/login.c
@@ -18,12 +18,7 @@
 #define COUNTER_STR_LENGH 2
 #define TIME_STR_LENGTH 14 // String with time in ms since Epoch and a \0
 #define MAX_LOGIN_ATTEMPTS 3
-
-#define LOGIN_SUCCESSFUL 0
-#define LOGIN_FAILED_FILE_ERROR 1
-#define LOGIN_FAILED_USER_UNKNOWN 2
-#define LOGIN_FAILED_USER_BLOCKED 3
-#define LOGIN_FAILED_WRONG_PASSWORD 4
+#define BLOCKED_USER_TIMER 5000
 
 // Function to trim newline characters
 void trim_newline(char* str) {
@@ -98,98 +93,12 @@ void extract_user_info(
             }
         }
     }
-    // printf("debug ---------- record in %s\n", FILE_HASHED_USERS);
-    // printf("debug file_username: %s\n", file_username);
-    // printf("debug file_salt_hex: %s\n", file_salt_hex);
-    // printf("debug file_hashed_password: %s\n", file_hashed_password);
-    // printf("debug file_counter_str: %s\n", file_counter_str);
-    // printf("debug file_time_str: %s\n", file_time_str);
 
     return;
 }
 
-// Function to check if username and password match an entry in users.txt; if
-// user is known, return failed login counter and time of last failed login
-// for that user
-int check_login(
-    const char* username,
-    const char* password,
-    unsigned int* counter,
-    unsigned long* time
-) {
-
-    FILE* file = fopen(FILE_HASHED_USERS, "r");
-    if (file == NULL) {
-        printf("Could not open hashed_users.txt\n");
-        return LOGIN_FAILED_FILE_ERROR;
-    }
-
-    char line[MAX_LINE_LENGTH];
-    char file_username[MAX_USERNAME_LENGTH];
-    char file_salt_hex[HEX_SALT_LENGTH];
-    char file_hashed_password[MAX_HASH_LENGTH];
-    char file_counter_str[COUNTER_STR_LENGH];
-    char file_time_str[TIME_STR_LENGTH];
-    unsigned char file_salt[SALT_LENGTH];
-    char hashed_password[MAX_HASH_LENGTH];
-    bool user_found = false;
-
-    while (fgets(line, sizeof(line), file)) {
-        extract_user_info(
-            line,
-            file_username,
-            file_salt_hex,
-            file_hashed_password,
-            file_counter_str,
-            file_time_str
-        );
-
-        // Compare entered username with the file's value
-        if (strcmp(username, file_username) == 0) {
-            user_found = true;
-            break;
-        }
-    }
-
-    fclose(file);
-
-    if (user_found) {
-        // Extract and return counter and time info for the user
-        printf("debug file_counter_str: %s\n", file_counter_str);
-        printf("debug file_time_str: %s\n", file_time_str);
-        *counter = atoi(file_counter_str);
-        *time = atol(file_time_str);
-        printf("debug counter: %u\n", *counter);
-        printf("debug time: %lu\n", *time);
-        
-        unsigned long time_ellapsed = get_milliseconds_since_epoch() - *time;
-        printf("debug time_ellapsed: %ld\n", time_ellapsed);
-
-        // If user tried too many times and didn't wait long enough, reject login
-        if (*counter >= (MAX_LOGIN_ATTEMPTS - 1)  && time_ellapsed < 5000) {
-            return LOGIN_FAILED_USER_BLOCKED;
-        }
-        else {
-            // Hash entered password together with file's salt
-            hex_to_bytes(file_salt_hex, file_salt);
-            hash_password(password, file_salt, hashed_password);
-    
-            // Compare entered password with the file's value
-            if (strcmp(hashed_password, file_hashed_password) == 0) {
-                return LOGIN_SUCCESSFUL;
-            }
-            else {
-                return LOGIN_FAILED_WRONG_PASSWORD;
-            }
-        }
-    }
-    else {
-        return LOGIN_FAILED_USER_UNKNOWN;
-    }
-}
-
-// Function to update the counter of failed login attempts in hashed_users.txt
-// for a given user, as well as the time of the last failed login
+// Function to update the counter of failed login attempts for a given user in
+// hashed_users.txt, as well as the time of the last failed login
 int update_failed_login_counter(char* username, int counter) {
     FILE* input_file = fopen(FILE_HASHED_USERS, "r");
     if (input_file == NULL) {
@@ -245,12 +154,113 @@ int update_failed_login_counter(char* username, int counter) {
     return 1;
 }
 
+// Function to decide if a login attempt is successful or not
+int check_login(const char* username, const char* password) {
+
+    FILE* file = fopen(FILE_HASHED_USERS, "r");
+    if (file == NULL) {
+        printf("Could not open hashed_users.txt\n");
+        return 0;
+    }
+
+    char line[MAX_LINE_LENGTH];
+    char file_username[MAX_USERNAME_LENGTH];
+    char file_salt_hex[HEX_SALT_LENGTH];
+    char file_hashed_password[MAX_HASH_LENGTH];
+    char file_counter_str[COUNTER_STR_LENGH];
+    char file_time_str[TIME_STR_LENGTH];
+    unsigned char file_salt[SALT_LENGTH];
+    char hashed_password[MAX_HASH_LENGTH];
+    bool user_found = false;
+
+    // Look up user in hash file
+    while (fgets(line, sizeof(line), file)) {
+        extract_user_info(
+            line,
+            file_username,
+            file_salt_hex,
+            file_hashed_password,
+            file_counter_str,
+            file_time_str
+        );
+
+        // Compare entered username with the file's value
+        if (strcmp(username, file_username) == 0) {
+            user_found = true;
+            break;
+        }
+    }
+
+    fclose(file);
+
+    // Decide whether to grant access
+    if (user_found) {
+        unsigned int counter = atoi(file_counter_str);
+        unsigned long time = atol(file_time_str);
+
+        // Hash entered password salted with the file's salt
+        hex_to_bytes(file_salt_hex, file_salt);
+        hash_password(password, file_salt, hashed_password);
+
+        // Compare entered password with the file's value
+        bool password_ok = (strcmp(hashed_password, file_hashed_password) == 0);
+
+        if (counter < MAX_LOGIN_ATTEMPTS - 1) {
+            if (password_ok) {
+                update_failed_login_counter(username, 0);
+                return 1;
+            }
+            else {
+                update_failed_login_counter(username, counter + 1);                
+                return 0;
+            }
+        }
+        else if (counter == MAX_LOGIN_ATTEMPTS - 1) {
+            if (password_ok) {
+                update_failed_login_counter(username, 0);
+                return 1;
+            }
+            else {
+                update_failed_login_counter(username, counter + 1);
+                printf(
+                    "Too many failed attempts. Please wait %0.1f seconds to retry.\n",
+                    BLOCKED_USER_TIMER / 1000.0
+                );
+                return 0;
+            }
+        }
+        else {  // counter >= MAX_LOGIN_ATTEMPTS
+            unsigned long time_ellapsed = get_milliseconds_since_epoch() - time;
+            bool timer_running = (time_ellapsed < BLOCKED_USER_TIMER);
+            
+            if (timer_running) {
+                printf(
+                    "Too many failed attempts. Please wait %0.1f seconds to retry.\n",
+                    (BLOCKED_USER_TIMER - time_ellapsed) / 1000.0
+                );
+                return 0;
+            }
+            else {
+                if (password_ok) {
+                    update_failed_login_counter(username, 0);
+                    return 1;
+                }
+                else {
+                    update_failed_login_counter(username, 1);
+                    return 0;
+                }
+            }
+        }
+    }
+    else {  // No user found
+        return 0;
+    }
+}
+
 int main() {
     char username[MAX_USERNAME_LENGTH];
     char password[MAX_PASSWORD_LENGTH];
     char command[MAX_COMMAND_LENGTH];
-    unsigned int counter = 0;
-    unsigned long time = 0;
 
     // Prompt user for username and password
     printf("Enter username: ");
@@ -260,14 +270,8 @@ int main() {
     fgets(password, sizeof(password), stdin);
     trim_newline(password);  // Remove newline character
 
-    // Check login credentials
-    int login_result = check_login(username, password, &counter, &time);
-
-    if (login_result == LOGIN_SUCCESSFUL) {
+    if (check_login(username, password)) {
         printf("Login successful!\n");
-
-        // Reset failed login counter for the user
-        update_failed_login_counter(username, 0);
 
         // Command prompt loop
         while (1) {
@@ -284,21 +288,6 @@ int main() {
     }
     else {
         printf("Login failed.\n");
-
-        if (login_result == LOGIN_FAILED_WRONG_PASSWORD) {
-            // Increment failed login counter for the user
-            if (counter < 9) {  // Avoid overflow, as counter is stored in only 1 char
-                ++counter;
-            }
-            update_failed_login_counter(username, counter);
-            printf("Login failed.\n");
-        }
-        else if (login_result == LOGIN_FAILED_USER_BLOCKED) {
-            unsigned long time_ellapsed = get_milliseconds_since_epoch() - time;
-            float time_left = (5000.0 - time_ellapsed) / 1000.0;
-// TODO: at 3rd attempt, this should print see exactly 5 secs, not 5 secs - time since last attempt...
-            printf("Too many failed attempts. Please wait %0.1f seconds to retry.\n", time_left);
-        }
     }
 
     return 0;
